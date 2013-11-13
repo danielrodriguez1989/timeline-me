ab7d1a0cb507a1af4f5d1486eff66d30f65d9e3c
ChristianCarruego <christian.carruego.74@gmail.com>
2013-11-13 14:31:43 -0300

    modificacion de la clase Agente

--- src/ar/edu/unlam/tallerweb/timelineme/Agente.java
+++ src/ar/edu/unlam/tallerweb/timelineme/Agente.java
@@ -2,10 +2,30 @@ package ar.edu.unlam.tallerweb.timelineme;
 
 public class Agente {
 	
+	private Long id;
 	private String nombre;
 	private String password;
-	private Long id;
-	private Long miEmpresa;
 	//private <List> empresaQueSigo;
+	//private <List> publicacionesQueHago;
 	
+	
+	public buscarEmpresa(){
+		
+	}
+	
+	public seguirEmpresa(){
+		
+	}
+	
+	public dejarDeSeguirEmpresa (Empresa empresa){
+		
+	}
+	
+	public publicaciones(){
+		
+	}
+	
+	public llamarPublicacion(<List> empresaQueSigo){
+		
+	}
 }
